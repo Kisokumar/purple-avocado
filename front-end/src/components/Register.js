1f1e37fc7bf36f12f0040c2ffdc6137be1714641
Kisho Mahadevan <kisokumar@fb.com>
2022-12-15 13:30:08 +0000

    feat: register button works

--- front-end/src/components/Register.js
+++ front-end/src/components/Register.js
@@ -1,32 +1,37 @@
-import "./App.css";
-
 function Register() {
   return (
     <section className="register">
       <form className="signupform">
         <h1 className="signup">Sign Up</h1>
-        <label for="fname">Full name: </label>
-        <input type="text" placeholder="Full name" className="input"></input>
-        <label for="fname">Username: </label>
-        <input type="text"
-          // onChange={(e) => setUsernameState(userref.current.value)}
-          // onChange={(e) => handleUsername()}
-          placeholder="Username"
-          className="field username input"
-        />
-        <label for="fname">Email:  </label>
-        <input type="text" placeholder="Email" className="input"></input>
-        <label for="fname">Password: </label>
-        <input type="text" placeholder="Password" className="input"></input>
-        <label for="fname">Re-enter Password: </label>
-        <input type="text" placeholder="Password" className="input"></input>
-        <button
-          type="button"
-          // onClick={handleSubmit}
-          className="submit register button"
-        >
-          Submit
-        </button>
+        <div className="signup-container">
+          {/* <label for="fname">Full name: </label> */}
+          <input type="text" placeholder="Full name" className="input"></input>
+          {/* <label for="fname">Username: </label> */}
+          <input
+            type="text"
+            // onChange={(e) => setUsernameState(userref.current.value)}
+            // onChange={(e) => handleUsername()}
+            placeholder="Username"
+            className="field username input"
+          />
+          {/* <label for="fname">Email: </label> */}
+          <input type="text" placeholder="Email" className="input"></input>
+          {/* <label for="fname">Password: </label> */}
+          <input type="text" placeholder="Password" className="input"></input>
+          {/* <label for="fname">Re-enter Password: </label> */}
+          <input
+            type="text"
+            placeholder="Re-enter password"
+            className="input"
+          ></input>
+          <button
+            type="button"
+            // onClick={handleSubmit}
+            className="submit button"
+          >
+            Submit
+          </button>
+        </div>
       </form>
     </section>
   );
