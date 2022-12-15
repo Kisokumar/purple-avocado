346ce55fa5d93b47bca51ea375c2cbe39dbf6a8c
Lucy Fox-Russell <lucyfoxrussell@fb.com>
2022-12-15 12:33:10 +0000

    sign-up page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
+import Register from "./components/Register";
 
 function App() {
   return <div className="App"></div>;
+  <Register></Register>
 }
 
 export default App;
---
+++ front-end/src/components/Register.js
@@ -0,0 +1,35 @@
+import "./App.css";
+
+function Register() {
+  return (
+    <section className="register">
+      <form className="signupform">
+        <h1 className="signup">Sign Up</h1>
+        <label for="fname">Full name: </label>
+        <input type="text" placeholder="Full name" className="field name input input"></input>
+        <label for="fname">Username: </label>
+        <input type="text"
+          // onChange={(e) => setUsernameState(userref.current.value)}
+          // onChange={(e) => handleUsername()}
+          placeholder="Username"
+          className="field username input"
+        />
+        <label for="fname">Email:  </label>
+        <input type="text" placeholder="Email" className="field emailinput input"></input>
+        <label for="fname">Password: </label>
+        <input type="text" placeholder="Password" className="field passwordinput input"></input>
+        <label for="fname">Re-enter Password: </label>
+        <input type="text" placeholder="Password" className="field passwordinput input"></input>
+        <button
+          type="button"
+          // onClick={handleSubmit}
+          className="submit register button"
+        >
+          Submit
+        </button>
+      </form>
+    </section>
+  );
+}
+
+export default Register;
